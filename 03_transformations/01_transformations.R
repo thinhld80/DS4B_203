a4053d947b3e34f57b0b0a987451587dcd09ce9f
David Espinola <davidespinola@Davids-MBP.attlocal.net>
2022-08-22 09:19:37 -0500

    Added code for transformations with log and box-cox.

---
+++ 03_transformations/01_transformations.R
@@ -0,0 +1,266 @@
+# BUSINESS SCIENCE UNIVERSITY
+# DS4B 203-R: ADVANCED TIME SERIES FORECASTING FOR BUSINESS
+# MODULE: TIME SERIES TRANSFORMATIONS -----
+
+# GOAL ----
+# - Exposure to Common Time Series Transformations
+
+# OBJECTIVES ----
+# - Variance Reduction - Log, Log1P, Box Cox
+# - Rolling & Smoothing
+# - Range Reduction - Normalization & Standardization
+# - Imputation & Outliers
+# - Lags & Differencing
+# - Fourier Series
+# - Confined Interval Forecasting
+
+
+
+# LIBRARIES ----
+
+library(tidyverse)
+library(timetk)
+library(lubridate)
+
+# DATA ----
+google_analytics_summary_tbl <- read_rds("00_data/google_analytics_summary_hourly.rds")
+google_analytics_summary_tbl 
+
+mailchimp_users_tbl <- read_rds("00_data/mailchimp_users.rds")
+mailchimp_users_tbl 
+
+transactions_tbl <- read_rds("00_data/transactions_weekly.rds")
+transactions_tbl 
+
+
+# DATA PREP ----
+
+google_analytics_summary_long_tbl <- google_analytics_summary_tbl %>%
+    mutate(date = ymd_h(dateHour)) %>%
+    select(-dateHour) %>%
+    pivot_longer(-date) %>%
+    group_by(name)
+
+subscribers_daily_tbl <- mailchimp_users_tbl %>%
+    summarise_by_time(optin_time, .by = "day", optins = n()) %>%
+    pad_by_time(.pad_value = 0, .start_date = "2018-06-01")
+
+
+
+# 1.0 VARIANCE REDUCTION ----
+
+# * Log & Log Plus 1----
+
+# ** Subscribers (Single)
+
+# No transformation
+
+subscribers_daily_tbl %>%
+  plot_time_series(optin_time, optins)
+
+subscribers_daily_tbl %>%
+  plot_time_series_regression(
+    .date_var = optin_time,
+    .formula  = optins ~ as.numeric(optin_time) + 
+      wday(optin_time, label = TRUE) +
+      month(optin_time, label = TRUE),
+    .show_summary = TRUE
+  )
+
+
+# Log - Show Error 
+
+subscribers_daily_tbl %>%
+  plot_time_series(optin_time, log(optins))
+
+# Log Plus 1
+
+subscribers_daily_tbl %>%
+  plot_time_series(optin_time, log1p(optins))
+
+# Inversion
+subscribers_daily_tbl %>%
+  plot_time_series(optin_time, log1p(optins) %>% expm1())
+
+
+# Benefit
+
+subscribers_daily_tbl %>%
+  plot_time_series_regression(
+    .date_var = optin_time,
+    .formula  = log1p(optins) ~ as.numeric(optin_time) + 
+      wday(optin_time, label = TRUE) +
+      month(optin_time, label = TRUE),
+    .show_summary = TRUE
+  )
+
+
+# Google Analytics (Groups)
+
+google_analytics_summary_long_tbl %>%
+  plot_time_series(date, log1p(value), name)
+
+
+# Inversion
+
+google_analytics_summary_long_tbl %>%
+  plot_time_series(date, expm1(log1p(value)), name)
+
+# * Box Cox ----
+
+# ** Subscribers (Single)
+
+# Box Cox Vec
+
+subscribers_daily_tbl %>%
+  plot_time_series(optin_time, box_cox_vec(optins + 1,lambda = "auto"))
+
+
+# Box Cox Inversion
+
+
+
+# ** Google Analytics (Groups)
+
+
+
+
+# 2.0 ROLLING & SMOOTHING ----
+# - Common time series operations to visualize trend
+# - A simple transformation that can help create improve features 
+# - Can help with outlier-effect reduction & trend detection
+# - Note: Businesses often use a rolling average as a forecasting technique 
+#   - A rolling average forecast is usually sub-optimal (good opportunity for you!)
+
+# * Sliding / Rolling Functions ----
+
+
+
+# * LOESS smoother ----
+
+
+
+# * Rolling Correlations ----
+# - Identify changing relationships
+
+
+
+
+# * Problem with Moving Avg Forecasting ----
+
+
+
+
+
+# 3.0 RANGE REDUCTION ----
+# - Used in visualization to overlay series
+# - Used in ML for models that are affected by feature magnitude (e.g. linear regression)
+
+# * Normalize to Range (0,1) ----
+# - INFO: recipes::step_range() is actually normalization to range(0,1)
+
+
+
+# * Standardize to Mean = 0 (Center), SD = 1 (Scaling) -----
+# - INFO: recipes::step_normalize() is actually standardization to mean = 0, sd = 1
+
+
+
+
+# 4.0 IMPUTING & OUTLIER CLEANING ----
+# - Imputation helps with filling gaps (if needed)
+# - Outlier removal helps linear regression detect trend and reduces high leverage points
+# WARNING: Make sure you check outliers against events 
+# - usually there is a reason for large values
+
+# * Imputation ----
+
+
+# * Cleaning (Imputation + Outlier Removal) ----
+
+
+
+# Outlier Effect - Before Cleaning
+
+
+
+# Outlier Effect - After Cleaning
+
+
+
+
+# 5.0 LAGS & DIFFERENCING -----
+# - Used to go from growth to change
+# - Makes a series "stationary" (potentially)
+# - MOST IMPORTANT - Can possibly use lagged variables in a model, if lags are correlated & 
+
+# * Lags ----
+# - Often used for feature engineering
+# - Autocorrelation
+# - 
+
+
+
+# * Differencing ----
+# - Makes a series "stationary"
+# - Used to get change
+#   - Stock price changes
+#   - Cumulative Revenue to change by day
+#   - Total subs to change by day
+
+# Cumulative Sum & Differencing
+
+
+
+# Comparing Differences 
+
+
+
+# Inversion 
+
+
+
+
+# 6.0 FOURIER SERIES ----
+# - Useful for incorporating seasonality & autocorrelation
+# - BENEFIT: Don't need a lag, just need a frequency (based on your time index)
+
+# * Vector (Single Fourier) ----
+
+
+# * Augmenting (Multiple Fourier Series) ----
+
+
+
+
+# 7.0 CONFINED INTERVAL FORECASTING ----
+# - Showcase: log_interval_vec()
+# - Transformation used to confine forecasts to a max/min interval
+
+# * Data ----
+
+
+
+# * Apply Transformation ----
+
+
+
+# * Model ----
+
+
+
+# * Create Future Data ----
+
+ 
+
+# * Predict ----
+
+
+
+# * Combine data ----
+
+
+
+# * Invert Transformation ----
+
+
