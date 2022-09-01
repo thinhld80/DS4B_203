635b5852c2d07039ac6882895e429637e5b15af2
David Espinola <davidespinola@Davids-MBP.attlocal.net>
2022-09-01 08:18:35 -0500

    Added code for confidence intervals and plotted untransformed data.

--- 03_transformations/01_transformations.R
+++ 03_transformations/01_transformations.R
@@ -455,8 +455,50 @@ predictions <- predict(object = model_fit_lm, future_tbl) %>% as.vector()
 predictions
 # * Combine data ----
 
+conf_interval <- 0.95
+residuals <- model_fit_lm$residuals %>% as.vector()
+alpha <- (1- conf_interval)/2
+1- alpha
 
+qnorm(alpha)
+qnorm(1-alpha)
+
+abs_margin_error <- abs(qnorm(alpha)* sd(residuals))
+
+forecast_tbl <- data_transformed_tbl %>%
+  select(optin_time, optins_trans) %>%
+  add_column(type = "actual") %>%
+  bind_rows(
+    future_tbl %>% 
+      select(optin_time) %>%
+      mutate(optins_trans = predictions,
+             type         = "prediction") %>%
+      mutate(
+        conf_lo = optins_trans - abs_margin_error,
+        conf_hi = optins_trans + abs_margin_error
+      )
+      
+  ) 
+
+forecast_tbl %>% tail()
+
+
+forecast_tbl %>%
+  pivot_longer(cols = c(optins_trans, conf_lo, conf_hi)) %>%
+  plot_time_series(optin_time, value, .color_var = name, .smooth = FALSE)
 
 # * Invert Transformation ----
 
-
+forecast_tbl %>%
+  pivot_longer(cols = c(optins_trans, conf_lo, conf_hi)) %>%
+  plot_time_series(
+    optin_time, 
+    log_interval_inv_vec(
+      x           = value,
+      limit_lower = limit_lower,
+      limit_upper = limit_upper,
+      offset      = offset
+    ),
+    .color_var = name,
+    .smooth    = FALSE
+  )
